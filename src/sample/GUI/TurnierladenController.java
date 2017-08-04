eedbc5487419d395633835e58f2ecf11462bf2ed
jens röcker <jens.roecker@hs-osnabrueck.de>
2017-08-04 13:08:54 +0200

    Doppelklick bei Turnierladen zur Auswahl

--- src/sample/GUI/TurnierladenController.java
+++ src/sample/GUI/TurnierladenController.java
@@ -1,15 +1,22 @@
 
 package sample.GUI;
 
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.util.Callback;
 import sample.DAO.TurnierDAO;
 import sample.DAO.TurnierDAOimpl;
@@ -23,70 +30,95 @@ import java.util.Dictionary;
 import java.util.ResourceBundle;
 
 
-
-
 /**
  * Created by jens on 03.08.2017.
  */
 
 public class TurnierladenController implements Initializable
 {
+
     @FXML
-    private TableView <Turnier> TurnierlisteTabelle;
+    public TableView TurnierlisteTabelle;
+
+    @FXML
+    public TableColumn Bearbeitung_TurnierLaden;
+
     @FXML
     public TableColumn TurnierDatumSpalte;
     @FXML
     public TableColumn TurnierNameSpalte;
     @FXML
     public TableColumn TurnierIDSpalte;
-
+    ObservableList<Turnier> turniere = FXCollections.observableArrayList();
     auswahlklasse a = new auswahlklasse();
     TurnierDAO t = new TurnierDAOimpl();
     Dictionary<Integer,Turnier> turnierliste = t.getAllTurniere();
 
-
     @FXML
-    public void auswahlSpeichern(ActionEvent event) throws Exception {
-        try {
-            System.out.println("Auswahl speichern");
-
-            if(TurnierlisteTabelle.getSelectionModel().getSelectedItem()!=null && (Turnier) TurnierlisteTabelle.getSelectionModel().getSelectedItem()!= a.getAktuelleTurnierAuswahl())
-            {
-                a.turnierAuswahlSpeichern((Turnier) TurnierlisteTabelle.getSelectionModel().getSelectedItem());
-                t.read(a.getAktuelleTurnierAuswahl());
-                System.out.println(a.getAktuelleTurnierAuswahl().getName());
-                a.turnierAuswahlSpeichern(a.getAktuelleTurnierAuswahl());
-                System.out.println("Das aktuelle Turnier lautet"+a.getAktuelleTurnierAuswahl().toString());
-                Main.getInstance().updateTitle("Badminton Turnierverwaltung - Turnier: "+a.getAktuelleTurnierAuswahl().getName());
-
-
-            }
-        }
-            catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     private void zeigeTabelle() {
         System.out.println("Print table");
-        ObservableList<Turnier> turniere = FXCollections.observableArrayList();
+
         for (int i = 1; i <= a.getTurnierliste().size(); i++) {
             turniere.add(a.getTurnierliste().get(i));
 
         }
         TurnierlisteTabelle.setItems(turniere);
+        TurnierIDSpalte.setCellValueFactory(new PropertyValueFactory<Turnier,Integer>("turnierid"));
+        //TurnierIDSpalte.setCellFactory(integerCellFactory);
+        TurnierDatumSpalte.setCellValueFactory(new PropertyValueFactory<Turnier,Date>("datum"));
+        TurnierNameSpalte.setCellValueFactory(new PropertyValueFactory<Turnier,String>("name"));
 
+        //TurnierDatumSpalte.setCellValueFactory(new PropertyValueFactory<Turnier, Date>("datum"));
+        //TurnierNameSpalte.setCellValueFactory(new PropertyValueFactory<Turnier, String>("name"));
 
-        TurnierDatumSpalte.setCellValueFactory(new PropertyValueFactory<Turnier, Date>("datum"));
-        TurnierNameSpalte.setCellValueFactory(new PropertyValueFactory<Turnier, String>("name"));
-        TurnierIDSpalte.setCellValueFactory(new PropertyValueFactory<Turnier, Integer>("turnierid"));
+        //TurnierIDSpalte.setCellValueFactory(new PropertyValueFactory<Turnier, Integer>("turnierid"));
 
-
-
-
-    }
+            }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         zeigeTabelle();
+        TurnierlisteTabelle.setRowFactory(tv -> {
+            TableRow row = new TableRow();
+            row.setOnMouseClicked(event -> {
+                if (! row.isEmpty() && event.getButton()== MouseButton.PRIMARY
+                        && event.getClickCount() == 2) {
+                    Turnier clickedRow = (Turnier) row.getItem();
+                    printRow(clickedRow);
+                    ((Node)(event.getSource())).getScene().getWindow().hide();
+                }
+                else if(! row.isEmpty() && event.getButton()== MouseButton.PRIMARY)
+                {
+                    System.out.println("KLICK");
+
+
+                }
+            });
+            return row ;
+        });
+    }
+    private void printRow(Turnier item) {
+
+        if(TurnierlisteTabelle.getSelectionModel().getSelectedItem()!=null && (Turnier) TurnierlisteTabelle.getSelectionModel().getSelectedItem()!= a.getAktuelleTurnierAuswahl())
+        {
+            a.turnierAuswahlSpeichern((Turnier) TurnierlisteTabelle.getSelectionModel().getSelectedItem());
+            t.read(a.getAktuelleTurnierAuswahl());
+            //System.out.println(a.getAktuelleTurnierAuswahl().getName());
+            a.turnierAuswahlSpeichern(a.getAktuelleTurnierAuswahl());
+            //System.out.println("Das aktuelle Turnier lautet"+a.getAktuelleTurnierAuswahl().toString());
+            Main.getInstance().updateTitle("Badminton Turnierverwaltung - Turnier: "+a.getAktuelleTurnierAuswahl().getName());
+            System.out.println("Turnierauswahl durch Doppelklick: = "+item.getName());
+            //((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+    }
+
+    class MyEventHandler implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent t) {
+            TableCell c = (TableCell) t.getSource();
+            int index = c.getIndex();
+            System.out.println("Name = " + turniere.get(index).getName());
+
+        }
     }
 }
