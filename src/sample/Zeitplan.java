e32f6dd4e80a790d1d122aef8c4215ac83451096
bruns1993 <florian@brunsgeeste.de>
2017-08-12 16:00:06 +0200

    Suchleiste Spieler funktioniert

---
+++ src/sample/Zeitplan.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Created by Florian-PC on 11.08.2017.
+ */
+public class Zeitplan {
+    private ObservableList<Spiel> zeitplan = FXCollections.observableArrayList();
+
+}
